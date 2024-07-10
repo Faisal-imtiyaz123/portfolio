08a1806c838430cb70e9a481f627f7ac7eaa4568
Faisal-imtiyaz123 <ee23btech11019@iith.ac.in>
2024-07-10 07:11:01 +0530

    fixed navabar styling

--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -26,15 +26,7 @@ function Navbar() {
   ]
   return (
     <nav className="bg-transparent">
-      <div className="flex items-center justify-between py-5">
-        <div className="flex flex-shrink-0 items-center">
-          <Link
-            href="/"
-            className=" text-[#16f2b3] text-3xl font-bold">
-            Faisal Imtiyaz
-          </Link>
-        </div>
-
+      <div className="flex items-center justify-center py-5">
         <ul  className="flex gap-8 ">
           {navLinks.map((navLink, index) => 
               <Link className="tracking-widest rounded-full  p-2 px-3 text-sm hover:bg-pink-500" key={index} href={navLink.link}>
